0627bdc0c0f3150b938852211635a6c3672e1484
Irina Ferkalek <irina.ferkalek@useagility.com>
2020-10-18 22:52:57 +0200

    deploy

---
+++ precache-manifest.10ab1441a135c38cd780985aa5ff0afc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b6283c062306f9cc3c61411117bce9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "43c8ab988e38fa1ce41c",
+    "url": "./static/css/main.8706159b.chunk.css"
+  },
+  {
+    "revision": "2dcb0abaf43a5a790838",
+    "url": "./static/js/2.aa8d267b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.aa8d267b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43c8ab988e38fa1ce41c",
+    "url": "./static/js/main.7d0d242b.chunk.js"
+  },
+  {
+    "revision": "9d98a42087b1fc0cffd6",
+    "url": "./static/js/runtime-main.a042af23.js"
+  }
+]);
